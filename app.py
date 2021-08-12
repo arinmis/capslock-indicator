2a363d4c20f06e5c90b05f32499d5e7e29a08c96
Arinmis <mustafa_arinmis@outlook.com>
2021-08-12 06:18:05 +0300

    capslock on and off pop ups created

--- app.py
+++ app.py
@@ -3,38 +3,13 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-# create window
-win = Gtk.Window()
 
+builder = Gtk.Builder()
+builder.add_from_file("glade-files/on.glade")
 
-# remove header bar
-hb = Gtk.HeaderBar()
-hb.set_show_close_button(False)
-win.set_titlebar(hb)
+window = builder.get_object("capslock-on")
+window.show_all()
 
-# capsloc images
-capslock_on_img = Gtk.Image()
-capslock_on_img.new_from_file("images/demo.png")
-
-#######################
-box = Gtk.Box()
-box.set_spacing(50)
-#box.set_orientation (Gtk.Orientation.VERTICAL)
-win.add(box)
-
-box.pack_start(capslock_on_img, True, True, 0)
-######################
-
-# show capslock on image
-def capslock_on(): 
-    win.show_all()  
-
-
-win.connect("destroy", Gtk.main_quit)
-# remove 
-win.show_all()
-
-#win.hide()
 Gtk.main()
 
 
